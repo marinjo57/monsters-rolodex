7d81cc929643678b1f8178e42da2f36433c9b233
Joseph Marin <jmarin@tripadvisor.com>
2020-01-09 10:06:53 -0500

    Updates

---
+++ precache-manifest.4e2c645002ea5dba4f0eecb2a0d0c4f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cdbad7427e6b14cddffea429843e43",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9aa324e7138ab2c8bd5a",
+    "url": "/monsters-rolodex/static/css/main.206bf019.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9aa324e7138ab2c8bd5a",
+    "url": "/monsters-rolodex/static/js/main.914bf66f.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
